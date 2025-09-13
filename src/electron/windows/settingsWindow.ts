ea718ef3053930bffd4c4b286bd0315f0590da7d
gillsB <78826213+gillsB@users.noreply.github.com>
2025-09-12 21:29:44 -0700

    Added minWidth/height to Settings window and ensuring it fits on screen.
    Added the height calculations from other windows to this to ensure that if the default height/width are too large to fit on screen it downsizes it.
    
    Also added a minWidth/Height as a minimum size viable for effectively using the window. This size is quite small so should work well regardless of the display a user is using as it is only 465x300 pixels unlike the backgroundSelect and other windows which have min values in the 500-600s

--- src/electron/windows/settingsWindow.ts
+++ src/electron/windows/settingsWindow.ts
@@ -1,10 +1,25 @@
+import { calculateSubWindowDimensions } from "../utils/util.js";
 import { openSubWindow } from "./subWindowManager.js";
 
 export function openSettingsWindow() {
+  const defaultWidth = 600;
+  const defaultHeight = 660;
+  const minWidth = 465;
+  const minHeight = 300;
+
+  const { actualWidth: calculatedWidth, actualHeight: calculatedHeight } =
+    calculateSubWindowDimensions(defaultWidth, defaultHeight);
+
+  // Ensure dimensions are at least as large as the minimum
+  const actualWidth = Math.max(calculatedWidth, minWidth);
+  const actualHeight = Math.max(calculatedHeight, minHeight);
+
   const options = {
-    width: 600,
-    height: 660,
+    width: actualWidth,
+    height: actualHeight,
     frame: false,
+    minWidth: minWidth,
+    minHeight: minHeight,
   };
 
   const subWindowHash = `settings`;
