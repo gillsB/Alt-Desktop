a88053804d93d794ce3da56d5fb56e34e2ad92a6
gillsB <78826213+gillsB@users.noreply.github.com>
2025-10-07 21:22:55 -0700

    Displaying data in IconsProfile (for testing).
    IconsProfile now loads the desktop unique files compared to current profile. It then lists the total amount of unique files, and then lists all unique files.
    
    This is mostly some debug for testing before the UI is started.

--- src/ui/components/IconsProfile.tsx
+++ src/ui/components/IconsProfile.tsx
@@ -1,19 +1,65 @@
+import { useEffect, useState } from "react";
 import { createLogger } from "../util/uiLogger";
 import { SubWindowHeader } from "./SubWindowHeader";
 
 const logger = createLogger("IconsProfile.tsx");
 
 const IconsProfile: React.FC = () => {
+  const [uniqueFiles, setUniqueFiles] = useState<desktopFile[]>([]);
+  const [profile, setProfile] = useState<string>("default");
   const handleClose = () => {
     window.electron.sendSubWindowAction("CLOSE_SUBWINDOW", "IconsProfile");
   };
+
+  useEffect(() => {
+    const fetchRendererStates = async () => {
+      const rendererStates = await window.electron.getRendererStates();
+      setProfile(rendererStates.profile || "");
+    };
+    fetchRendererStates();
+  }, []);
+
+  useEffect(() => {
+    const updateStates = (...args: unknown[]) => {
+      const state = args[1] as Partial<RendererStates>;
+      if ("profile" in state) {
+        setProfile(state.profile || "default");
+      }
+    };
+    window.electron.on("renderer-state-updated", updateStates);
+    return () => {
+      window.electron.off("renderer-state-updated", updateStates);
+    };
+  }, []);
+
+  useEffect(() => {
+    const fetchUniqueFiles = async () => {
+      try {
+        setUniqueFiles(await window.electron.getDesktopUniqueFiles(profile));
+      } catch (error) {
+        logger.error("Error fetching unique desktop files:", error);
+      }
+    };
+    fetchUniqueFiles();
+  }, [profile]);
 
   logger.info("Rendering IconsProfile component");
 
   return (
     <div className="subwindow-container">
       <SubWindowHeader title={`Icons Profile`} onClose={handleClose} />
-      <label>IconsProfile</label>
+      <div>Total unique DesktopFiles found: {uniqueFiles.length}</div>
+
+      <div
+        style={{
+          overflowY: "auto",
+          overflowX: "hidden",
+        }}
+      >
+        {uniqueFiles.map((file, index) => (
+          <div key={index}>{JSON.stringify(file)}</div>
+        ))}
+      </div>
     </div>
   );
 };
